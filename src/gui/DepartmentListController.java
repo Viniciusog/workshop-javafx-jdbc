fb0163a09f4b52aa50e4c0b7d6d9c738f5677a4a
Viniciusog <vinijipa23@gmail.com>
2019-07-05 11:22:14 -0400

    Observer design pattern to update tableview

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
 import javafx.collections.FXCollections;
@@ -26,92 +27,106 @@ import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
 
-public class DepartmentListController implements Initializable{
+public class DepartmentListController implements Initializable, DataChangeListener {
 
 	private DepartmentService service;
 	@FXML
 	private TableView<Department> tableViewDepartment;
-	
-	//Tipo da table e o tipo da coluna desejada
+
+	// Tipo da table e o tipo da coluna desejada
 	@FXML
 	private TableColumn<Department, Integer> tableColumnId;
-	
+
 	@FXML
 	private TableColumn<Department, String> tableColumnName;
-	
+
 	@FXML
 	private Button btNew;
-	
-	
+
 	private ObservableList<Department> obsList;
-	
+
 	@FXML
 	public void onBtNewAction(ActionEvent event) {
 		Stage parentStage = Utils.currentStage(event);
 		Department obj = new Department();
-		//Nome do objeto do departamento, nome da tela que vou carregar e o Stage da tela atual
+		// Nome do objeto do departamento, nome da tela que vou carregar e o Stage da
+		// tela atual
 		createDialogForm(obj, "/gui/DepartmentForm.fxml", parentStage);
 	}
-	
-	//Forma certa de injetar dependencia 
+
+	// Forma certa de injetar dependencia
 	public void setDepartmentService(DepartmentService service) {
 		this.service = service;
 	}
-	
+
 	@Override
-	public void initialize(URL url, ResourceBundle rb) {	
+	public void initialize(URL url, ResourceBundle rb) {
 		initializeNodes();
 	}
+
 	private void initializeNodes() {
-		//Padrão javafx para iniciar o comportamento das colunas
+		// Padrão javafx para iniciar o comportamento das colunas
 		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
 		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
-		
-		//Fazer table view acompanhar a altura da janela - class 250
+
+		// Fazer table view acompanhar a altura da janela - class 250
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
-		
+
 	}
-	
+
 	public void updateTableView() {
-		if(service == null) {
+		if (service == null) {
 			throw new IllegalStateException("Service was null");
 		}
-		
-		//Carregando itens e mostrando na table view
+
+		// Carregando itens e mostrando na table view
 		List<Department> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewDepartment.setItems(obsList);
-		
+
 	}
 	
 	private void createDialogForm(Department obj, String absoluteName, Stage parentStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			Pane pane = loader.load();
-			
-			//Pegar o controlador da tela que eu acabei de carregar, que no caso é o DepartmentForm.fxml
-			//Depois carregar os dados do objeto (No caso o obj) no formulario
+
+			// Pegar o controlador da tela que eu acabei de carregar, que no caso é o
+			// DepartmentForm.fxml
+			// Depois carregar os dados do objeto (No caso o obj) no formulario
 			DepartmentFormController controller = loader.getController();
 			controller.setDepartment(obj);
 			controller.setDepartmentService(new DepartmentService());
+			controller.subscribeDataChangeListener(this);  //Estou me inscrevendo pra receber o evento
 			controller.updateFormData();
-			
-			/*Função para carregar a janela do formulario para preencher um novo departamento*/
-			
+
+			/*
+			 * Função para carregar a janela do formulario para preencher um novo
+			 * departamento
+			 */
+
 			Stage dialogStage = new Stage();
 			dialogStage.setTitle("Enter department data");
 			dialogStage.setScene(new Scene(pane));
 			dialogStage.setResizable(false);
-			//Quem é o stage pai dessa janela? É o parent Stage
-			dialogStage.initOwner(parentStage); 
-			//Enquanto você não fechar esta janela, vc n poderá acessar a janela anterior
-			dialogStage.initModality(Modality.WINDOW_MODAL); 
+			// Quem é o stage pai dessa janela? É o parent Stage
+			dialogStage.initOwner(parentStage);
+			// Enquanto você não fechar esta janela, vc n poderá acessar a janela anterior
+			dialogStage.initModality(Modality.WINDOW_MODAL);
 			dialogStage.showAndWait();
-			
-		} 
-		 catch(IOException e) {
+
+		}
+
+		catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Erro loading view", e.getMessage(), AlertType.ERROR);
 		}
+	}
+
+	//Quando evento for disparado, será executado o método updataTableView
+	@Override
+	public void onDataChanged() {
+		updateTableView();
+
 	}
 }
---
+++ src/gui/listeners/DataChangeListener.java
@@ -0,0 +1,8 @@
+package gui.listeners;
+
+public interface DataChangeListener {
+	
+	//Evento pra ser disparado quando os dados mudarem
+	void onDataChanged();
+
+}
